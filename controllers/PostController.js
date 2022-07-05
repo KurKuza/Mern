e4121a0680b2c5ddeaf421a1bc386bd42c07aace
Kurkuza <danilsherrbakov1993@gmail.com>
2022-07-06 01:19:54 +0300

    Crud

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -29,19 +29,74 @@ export const getOne = async (req, res) => {
 					)
 				}
 
-				if (!doc) return res.status(404).json({
-					message: 'Статья не найдена'
-				})
+				if (!doc) {
+					return res.status(404).json({
+						message: 'Статья не найдена'
+					})
+				}
 
 				res.json(doc)
 			}
-
 		)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json(
+			{ message: 'Не удалось получить статью' }
+		)
+	}
+}
+
+export const remove = async (req, res) => {
+	try {
+		const postId = req.params.id
+
+		PostModel.findOneAndDelete({
+			_id: postId
+		}, (err, doc) => {
+			if (err) {
+				console.log(err)
+				return res.status(500).json({
+					message: 'Не удалось удалить статью'
+				})
+			}
+			if (!doc) {
+				return res.status(404).json({
+					message: 'Статья не найдена'
+				})
+			}
+
+			res.json({ success: true })
+		})
 
 	} catch (err) {
 		console.log(err)
 		res.status(500).json(
-			{ message: 'Не удалось получить статьи' }
+			{ message: 'Не удалось получить статью' }
+		)
+	}
+}
+
+export const update = async (req, res) => {
+	try {
+		const postId = req.params.id
+
+		await PostModel.updateOne(
+			{ _id: postId },
+			{
+				title: req.body.title,
+				text: req.body.text,
+				imageUrl: req.body.imageUrl,
+				tags: req.body.tags,
+				user: req.userId
+			})
+
+		res.json({
+			success: true
+		})
+	} catch (err) {
+		console.log(err)
+		res.status(500).json(
+			{ message: 'Не удалось обновить статью' }
 		)
 	}
 }
