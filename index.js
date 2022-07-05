706ca198268a05c80d253071b5a067dfaa290355
Kurkuza <danilsherrbakov1993@gmail.com>
2022-07-05 09:40:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from 'express'
+import { validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose'
+import { registerValidation } from './validations/auth.js'
+import UserModel from './models/User.js'
+
+mongoose
+	.connect('mongodb+srv://admin:wwwwww@cluster0.z99sxnx.mongodb.net/?retryWrites=true&w=majority')
+	.then(() => console.log('DB ok'))
+	.catch((err) => { 'DB error', console.log(err) })
+
+const app = express()
+
+app.use(express.json())
+
+
+app.post('/auth/register', registerValidation, async (req, res) => {
+	try {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) return res.status(400).json(errors.array())
+
+		const password = req.body.password
+		const salt = await bcrypt.genSalt(10)
+		const passwordHash = await bcrypt.hash(password, salt)
+
+
+		const doc = new UserModel({
+			email: req.body.email,
+			fullName: req.body.fullName,
+			avatarUrl: req.body.avatarUrl,
+			passwordHash,
+		})
+		const user = await doc.save()
+
+		const tocen = jwt.sign({
+			_id: user._id
+		},
+			'secret123',
+			{ expiresIn: '30d' })
+
+		res.json(user, tocen)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json(
+			{ message: 'Не удалось зарегистрироваться' }
+		)
+
+	}
+})
+
+app.listen(4444, (err) => {
+	err && console.log(err)
+
+	console.log('Server OK')
+})
