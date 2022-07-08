996cdf522e7d0c159117f8b486659dd553db937a
Kurkuza <danilsherrbakov1993@gmail.com>
2022-07-08 17:56:30 +0300

    dev

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import multer from 'multer'
+import cors from 'cors'
 
 import mongoose from 'mongoose'
 
 import { loginValidation, postCreateValidation, registerValidation } from './validations.js'
 
-import { checkAuth, handleValidationErrors } from './utils/indext.js'
+import { checkAuth, handleValidationErrors } from './utils/index.js'
 
 import { UserController, PostController } from './controllers/index.js'
 
@@ -27,6 +28,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 app.use(express.json())
+app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
@@ -39,7 +41,11 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 	})
 })
 
+app.get('/tags', PostController.getLastTags)
+
+
 app.get('/posts', PostController.getAll)
+app.get('/posts/tags', PostController.getLastTags)
 app.get('/posts/:id', PostController.getOne)
 app.post('/posts', checkAuth, postCreateValidation, PostController.create)
 app.delete('/posts/:id', checkAuth, PostController.remove)
